a012814d320de2394e99a084d86794384008ddc9
Amsakan Bavan <amsakan.bavan@softwareag.com>
2024-05-23 18:10:43 +0200

    Added exercise 4

---
+++ ex4/src/main/java/com/example/Matrix.java
@@ -0,0 +1,76 @@
+package com.example;
+
+public class Matrix <T extends Number> {
+    private int columns;
+    private int rows;
+    private T[][] matrix;
+
+    public Matrix(int rows, int columns) {
+        this.columns = columns;
+        this.rows = rows;
+        this.matrix = (T[][])new Number[rows][columns];
+    }
+
+    public void set(T value, int i, int j) throws IllegalArgumentException {
+        if(i >= rows || j >= columns || i < 0 || j < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        this.matrix[i][j] = value;
+    }
+
+    public T get(int i, int j) throws IllegalArgumentException {
+        if(i >= rows || j >= columns || i < 0 || j < 0) {
+            throw new IllegalArgumentException();
+        }
+
+        return this.matrix[i][j];
+    }
+
+
+    public Matrix<? extends Number> add(Matrix<T> b) throws IllegalArgumentException {
+        if(this.rows != b.getRows() || this.columns != b.getColumns()) {
+            throw new IllegalArgumentException();
+        }
+
+        Matrix<Double> res = new Matrix<>(rows, columns);
+        for(int i = 0; i < this.rows; i++) {
+            for(int j = 0; j < this.columns; j++) {
+                double sum = this.get(i,j).doubleValue() + b.get(i, j).doubleValue();
+                res.set(sum, i, j);
+            }
+        }
+
+        return res;
+    }
+
+    public Matrix<? extends Number> scale(T scale) {
+        Matrix<Double> res = new Matrix<>(this.rows, this.columns);
+
+        for(int i = 0; i < this.rows; i++) {
+            for(int j = 0; j < this.columns; j++) {
+                double product = this.get(i,j).doubleValue() * scale.doubleValue();
+                res.set(product, i, j);
+            }
+        }
+
+        return res;
+    }
+
+    public void print() {
+        for(int i = 0; i < this.rows; i++) {
+            for(int j = 0; j < this.columns; j++) {
+                System.out.println(this.get(i, j));
+            }
+            System.out.println();
+        }
+    }
+
+    public int getColumns() {
+        return columns;
+    }
+
+    public int getRows() {
+        return rows;
+    }
+}
---
+++ ex4/src/test/java/MatrixTests.java
@@ -0,0 +1,78 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import com.example.Matrix;
+
+public class MatrixTests {
+    private Matrix<Integer> testMatrix;
+    private Matrix<Integer> testMatrix2;
+
+    @BeforeEach
+    private void setUp() {
+        this.testMatrix = new Matrix<>(2, 2);
+        this.testMatrix2 = new Matrix<>(2, 2);
+    }
+
+    @Test
+    public void testSetAndGet() {
+        int x = 0;
+
+        for(int i = 0; i < testMatrix.getRows(); i++) {
+            for(int j = 0; j < testMatrix.getColumns(); j++) {
+                testMatrix.set(x, i, j);
+                x++;
+            }
+        }
+
+        x = 0;
+        for(int i = 0; i < testMatrix.getRows(); i++) {
+            for(int j = 0; j < testMatrix.getColumns(); j++) {
+                assertEquals(x, testMatrix.get(i, j));
+                x++;
+            }
+        }
+    }
+
+    @Test
+    public void testAdd() {
+        int x = 0;
+        for(int i = 0; i < testMatrix.getRows(); i++) {
+            for(int j = 0; j < testMatrix.getColumns(); j++) {
+                testMatrix.set(x, i, j);
+                testMatrix2.set(x, i, j);
+                x++;
+            }
+        }
+
+        Matrix<? extends Number> sum = testMatrix.add(testMatrix2);
+        x = 0;
+        for(int i = 0; i < testMatrix.getRows(); i++) {
+            for(int j = 0; j < testMatrix.getColumns(); j++) {
+                assertEquals((double)x+x, sum.get(i, j));
+                x++;
+            }
+        }
+    }
+
+    @Test
+    public void testScale() {
+        int x = 0;
+        for(int i = 0; i < testMatrix.getRows(); i++) {
+            for(int j = 0; j < testMatrix.getColumns(); j++) {
+                testMatrix.set(x, i, j);
+                x++;
+            }
+        }
+
+        Matrix<? extends Number> scaledMatrix = testMatrix.scale(3);
+        x = 0;
+        for(int i = 0; i < testMatrix.getRows(); i++) {
+            for(int j = 0; j < testMatrix.getColumns(); j++) {
+                assertEquals((double)3*x, scaledMatrix.get(i, j));
+                x++;
+            }
+        }
+    }
+}
